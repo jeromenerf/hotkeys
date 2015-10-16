961e0ca681b34478344c7b6e34273d0bbd63305b
jeromenerf <jerome.andrieux@gmail.com>
2015-10-16 20:02:59 +0200

    Gardening

--- main.go
+++ main.go
@@ -1,11 +1,3 @@
-// Example window-names fetches a list of all top-level client windows managed
-// by the currently running window manager, and prints the name and size
-// of each window.
-//
-// This example demonstrates how to use some aspects of the ewmh and icccm
-// packages. It also shows how to use the xwindow package to find the
-// geometry of a client window. In particular, finding the geometry is
-// intelligent, as it includes the geometry of the decorations if they exist.
 package main
 
 import (
